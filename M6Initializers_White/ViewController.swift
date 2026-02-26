02c6ea77f38b2313ec4e8272009ac9ac05ba3c6c
james_white249 <james_white249@mymail.eku.edu>
2026-02-26 12:18:04 -0500

    initial commit

--- M6Initializers_White/ViewController.swift
+++ M6Initializers_White/ViewController.swift
@@ -9,9 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var modeControl: UISegmentedControl!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBAction func resultTapped(_ sender: UIButton) {
+    }
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ageField.keyboardType = .numberPad
+    }
+    
+    //Models
+    class Person{
+        var name: String
+        var age: Int
+        
+        //Designated Init
+        init(name: String, age: Int) {
+            self.name = name
+            self.age = age
+        }
+        
+        convenience init(){
+            self.init(name: "John", age: 23)
+        }
+        
+        convenience init(name: String){
+            self.init(name: name, age: 21)
+        }
+        
+        convenience init(name: String, age: Int){
+            self.init(name: name, age: age)
+        }
+    }
+    
+    class ValidatedPerson{
+        let name: String
+        let age: Int
+        
+        inti?(name: String, age: Int){
+            guard !name.isEmpty, age > 0 else { return nil }
+        }
     }
 
 
