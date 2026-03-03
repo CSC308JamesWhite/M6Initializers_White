2ae0c569c9f34a141734a65e35af1d933de9cb38
james-white249 <james_white249@mymail.eku.edu>
2026-03-03 11:20:39 -0500

    Update ViewController.swift

--- M6Initializers_White/ViewController.swift
+++ M6Initializers_White/ViewController.swift
@@ -13,6 +13,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var ageField: UITextField!
     @IBOutlet weak var nameField: UITextField!
     @IBAction func resultTapped(_ sender: UIButton) {
+        let nameRaw = nameField.text ?? ""
+        let ageRaw = ageField.text ?? ""
+
+        switch modeControl.selectedSegmentIndex{
+            case 0://designated init
+                guard !nameRaw.isEmpty, !ageRaw.isEmpty else{
+                    showAlert(title: "Missing input", message: "Enter name and age.")
+                    return
+                }
+                guard let age = Int(ageRaw) else{
+                    showAlert(title: "Invalid input", message: "Age must be a number.")
+                    return
+                }
+                let p = Person(name: nameRaw, age: ageRaw)
+                resultLabel.text = "Designated -> \(p.name), \(p.age)"
+            case 1://convenience init
+                if nameRaw.isEmpty{
+                    let p = Person()
+                    resultLabel.text = "Convenience 1 -> \(p.name), \(p.age)"
+                    return
+                }
+            case 2:
+                gurad !nameRaw.isEmpty, !ageRaw.isEmpty else{
+                    showAlert(title: "Missing input", message: "Enter both your name and age.")
+                    return
+                }
+                if let vp = ValidatedPerson(name: nameRaw, age: age){
+                    resultLabel.text = "Failable -> \(vp.name), \(vp.age)"
+                }
+            
+            default:
+                break
+        }
+    }
+    func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated:true)
     }
     @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
